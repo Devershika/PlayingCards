32f217d5c2771772a949e11b5644dbaa8e6003ce
Devershika Mohane <142586782+Devershika@users.noreply.github.com>
2025-03-05 21:16:53 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,3 +10,19 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Deck deck = new Deck();
         int choice;
+
+        
+        do {
+            System.out.println("\n********** MENU **********");
+            System.out.println("1. Print Deck");
+            System.out.println("2. Shuffle Deck");
+            System.out.println("3. Print a Card");
+            System.out.println("4. Find a Card");
+            System.out.println("5. Find Same Suit Cards");
+            System.out.println("6. Find Same Rank Cards");
+            System.out.println("7. Deal 5 Random Cards");
+            System.out.println("8. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+
