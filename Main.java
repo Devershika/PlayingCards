7eba0980e8d095139dcd79281e1aba8dc4a9044e
Devershika Mohane <142586782+Devershika@users.noreply.github.com>
2025-03-05 21:12:09 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+//Devershika Mohane
+//23070126032
+//AIML A2
+
+// Main.java
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        Deck deck = new Deck();
+        int choice;
