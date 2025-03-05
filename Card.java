7a87052ddbf12bdbef0480d6ab0f69924c42db53
Devershika Mohane <142586782+Devershika@users.noreply.github.com>
2025-03-05 21:19:34 +0530

    Update Card.java

--- Card.java
+++ Card.java
@@ -7,3 +7,11 @@ public class Card {
         this.suit = suit;
         this.rank = rank;
     }
+    
+    public String getSuit() {
+        return suit;
+    }
+
+    public String getRank() {
+        return rank;
+    }
